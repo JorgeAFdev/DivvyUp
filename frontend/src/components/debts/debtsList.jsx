9db395057b986fee40889b7d19561b57c71d1437
Jorge <jorgealvarezfrancino@gmail.com>
2025-03-18 15:34:13 +0100

    feat(debtsList): pass refreshGroupDetails prop to Debt component
    - Pass `refreshGroupDetails` prop to `Debt` component

--- frontend/src/components/debts/debtsList.jsx
+++ frontend/src/components/debts/debtsList.jsx
@@ -1,14 +1,14 @@
 import styles from './debtsList.module.css'
 import Debt from "./debt/debt";
 
-const DebtsList = ({ groupDebts }) => {
+const DebtsList = ({ groupDebts, refreshGroupDetails }) => {
     return (
         <div className={styles.debtsList}>
             <div>
                 {groupDebts?.length === 0 ? '' : (
                     <ul className={styles.list}>
                         {groupDebts?.map((debt) => (
-                            <Debt key={debt._id} debt={debt} />
+                            <Debt key={debt._id} debt={debt} refreshGroupDetails={refreshGroupDetails} />
                         ))}
                     </ul>
                 )}
