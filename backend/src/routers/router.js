58b1f20f53fba93f58c027ccf4b442052702d2cc
Jorge <jorgealvarezfrancino@gmail.com>
2025-03-18 10:16:00 +0100

    feat(router): add payment routes to main router
    - Import `payment.routes` and integrate it into the main router

--- backend/src/routers/router.js
+++ backend/src/routers/router.js
@@ -3,6 +3,7 @@ const userRoutes = require("./user.routes")
 const groupExpensesRouter = require("./expense.routes");
 const groupRoutes = require("./group.routes");
 const authRouter = require("./auth.routes")
+const paymentRoutes = require("./payment.routes")
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/group", groupExpensesRouter);
 router.use("/user", userRoutes);
 router.use("/group", groupRoutes);
 router.use('/auth', authRouter);
+router.use('/payment', paymentRoutes);
 
 module.exports = router;
