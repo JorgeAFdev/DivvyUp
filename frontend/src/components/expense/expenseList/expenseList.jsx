e7638d52de9341bcfb14a8e00b53750d53bf78d5
Jorge <jorgealvarezfrancino@gmail.com>
2025-03-20 16:32:00 +0100

    feat(expense): add className prop to ExpenseList for customizable styling

--- frontend/src/components/expense/expenseList/expenseList.jsx
+++ frontend/src/components/expense/expenseList/expenseList.jsx
@@ -1,13 +1,12 @@
 import styles from './expenseList.module.css'
 import Expense from '../expense/expense';
 
-const ExpenseList = ({ groupExpenses, refreshGroupDetails }) => {
-
+const ExpenseList = ({ groupExpenses, refreshGroupDetails, className }) => {
     return (
         <section className={styles.expenses}>
             <div>
                 {groupExpenses?.length === 0 ? (<p>There are no expenses in this group</p>) : (
-                    <ul className={styles.list}>
+                    <ul className={className ? [styles[className]] : styles.detailsList}>
                         {groupExpenses?.map((item) => (
                             <Expense key={item._id} expense={item} refreshGroupDetails={refreshGroupDetails} />
                         ))}
