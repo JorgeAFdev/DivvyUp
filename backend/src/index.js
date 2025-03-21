795011396958e3b8f190990423cd3b1279517227
Jorge <jorgealvarezfrancino@gmail.com>
2025-03-21 15:47:39 +0100

    set up socket.io server with Express integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const { connectDB } = require("./mongo/connection");
+const http = require('http');
 const cors = require("cors");
+const { socketServer } = require('./socket/socket.server')
 const app = express();
 const router = require("./routers/router");
 
 app.use(cors());
 app.use(express.json());
+
+const server = http.createServer(app);
+const io = socketServer(server);
+
+app.set('socketio', io);
 
 app.use("/api", router);
 
@@ -13,7 +20,7 @@ connectDB().then(() => console.log("Connected to database!"));
 
 const port = process.env.PORT || 3001;
 
-const server = app.listen(port, () => {
+server.listen(port, () => {
   console.log("Server is up and running ⚡");
 });
 
