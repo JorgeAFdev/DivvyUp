0e98fb77e39507421c4abc4320062ac419bcbd4d
Jorge <jorgealvarezfrancino@gmail.com>
2025-03-25 20:52:34 +0100

    feat(expenseList): add optional title for expenses section in expense list component

--- frontend/src/components/expense/expenseList/expenseList.jsx
+++ frontend/src/components/expense/expenseList/expenseList.jsx
@@ -1,16 +1,19 @@
 import styles from './expenseList.module.css'
 import Expense from '../expense/expense';
 
-const ExpenseList = ({ groupExpenses, refreshGroupDetails, className }) => {
+const ExpenseList = ({ groupExpenses, refreshGroupDetails, className, title = true }) => {
     return (
         <section className={styles.expenses}>
             <div>
                 {groupExpenses?.length === 0 ? (<p>There are no expenses in this group</p>) : (
-                    <ul className={className ? [styles[className]] : styles.detailsList}>
-                        {groupExpenses?.map((item) => (
-                            <Expense key={item._id} expense={item} refreshGroupDetails={refreshGroupDetails} />
-                        ))}
-                    </ul>
+                    <>
+                        {title && <h2 className={styles.title}>Expenses</h2>}
+                        <ul className={className ? [styles[className]] : styles.detailsList}>
+                            {groupExpenses?.map((item) => (
+                                <Expense key={item._id} expense={item} refreshGroupDetails={refreshGroupDetails} />
+                            ))}
+                        </ul>
+                    </>
                 )}
             </div>
         </section>
