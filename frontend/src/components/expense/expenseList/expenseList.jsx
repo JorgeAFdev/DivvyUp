5d917d39f1cdc7ba87272d9d4b92af772ee8b80b
Jorge <jorgealvarezfrancino@gmail.com>
2025-03-26 23:16:44 +0100

    feat(expenseList): add dark mode support for loading and error messages

--- frontend/src/components/expense/expenseList/expenseList.jsx
+++ frontend/src/components/expense/expenseList/expenseList.jsx
@@ -1,11 +1,14 @@
 import styles from './expenseList.module.css'
 import Expense from '../expense/expense';
+import { useDarkMode } from '../../../context/darkModeContext';
 
 const ExpenseList = ({ groupExpenses, refreshGroupDetails, className, title = true }) => {
+    const { darkMode } = useDarkMode();
+
     return (
         <section className={styles.expenses}>
             <div>
-                {groupExpenses?.length === 0 ? (<p>There are no expenses in this group</p>) : (
+                {groupExpenses?.length === 0 ? (<p className={darkMode ? styles.text : ''} >There are no expenses in this group</p>) : (
                     <>
                         {title && <h2 className={styles.title}>Expenses</h2>}
                         <ul className={className ? [styles[className]] : styles.detailsList}>
