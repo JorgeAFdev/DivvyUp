ccccb7b70937cb00b6299c846035be0c668fb17f
biescazzz <tucrushfav@gmail.com>
2025-03-27 18:28:49 +0100

    actualizacion pagina 404 error

--- frontend/src/pages/noMatch/noMatch.jsx
+++ frontend/src/pages/noMatch/noMatch.jsx
@@ -3,7 +3,23 @@
 const Groups = () => {
 
     return (
-        <p>This page does not exist</p>
+        <div style={{ textAlign: 'center', marginTop: '10vh' }}>
+        <div style={{ 
+            display: 'flex', 
+            flexDirection: 'column',
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            width: '600px', 
+            height: '600px', 
+            borderRadius: '50%', 
+            border: '6px solid #1e99fa', 
+            padding: '20px', 
+            margin: '0 auto'
+        }}>
+            <h1 style={{ color: '#1e99fa', fontSize: '5rem', fontWeight: 'bold', margin: '0' }}>ERROR 404</h1>
+            <p style={{ color: '#1e99fa', fontSize: '1.2rem', marginTop: '10px' }}>This page does not exist</p>
+        </div>
+    </div>
     )
 };
 
