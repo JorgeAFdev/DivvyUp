d4f1c1fef56bb94e2b03219503f234960a5e318e
Jorge <jorgealvarezfrancino@gmail.com>
2025-11-13 11:22:50 +0100

    refactor(theme): remove dark mode handling from ExoenseList component
    - Removed `useDarkMode` import and conditional dark mode class in JSX
    - Debt component now relies solely on default CSS module classes
    - Simplifies component by removing theme-specific logic

--- frontend/src/components/expense/expenseList/expenseList.jsx
+++ frontend/src/components/expense/expenseList/expenseList.jsx
@@ -1,14 +1,12 @@
 import styles from './expenseList.module.css'
 import Expense from '../expense/expense';
-import { useDarkMode } from '../../../context/darkModeContext';
 
 const ExpenseList = ({ groupExpenses, refreshGroupDetails, className, title = true }) => {
-    const { darkMode } = useDarkMode();
 
     return (
         <section className={styles.expenses}>
             <div>
-                {groupExpenses?.length === 0 ? (<p className={darkMode ? styles.text : ''} >There are no expenses in this group</p>) : (
+                {groupExpenses?.length === 0 ? (<p className={styles.text} >There are no expenses in this group</p>) : (
                     <>
                         {title && <h2 className={styles.title}>Expenses</h2>}
                         <ul className={className ? [styles[className]] : styles.detailsList}>
